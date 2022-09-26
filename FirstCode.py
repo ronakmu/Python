2cbb9693da18e9d7a40e24751727be4896e459d1
ronakmu <70596862+ronakmu@users.noreply.github.com>
2022-09-26 20:51:50 +0530

    Add files via upload

---
+++ FirstCode.py
@@ -0,0 +1,10 @@
+print("First Python Code\n")
+#Function Definition
+def main():
+	
+	print("Inside Main\n")
+	print("***___***Jay Ganesh***___***\n")
+	
+if __name__=="__main__":	
+	main()		#Function Call
+     
