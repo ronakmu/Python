8c0cc75154cde621e23d9c3569255afe887f1b7b
ronakmu <70596862+ronakmu@users.noreply.github.com>
2022-09-26 21:05:44 +0530

    Add files via upload

---
+++ AdditionModule.py
@@ -0,0 +1,13 @@
+import PrintModule
+
+def Add(value1,value2):
+
+		print("Value of __name__ from Addition function is :",__name__)
+		Ans = int(value1) + int(value2)
+		return Ans
+
+def Sub(value1,value2):
+
+		print("Value of __name__ from Subtraction function is :",__name__)
+		Ans = int(value1) - int(value2)
+		return Ans
